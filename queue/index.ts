e7d4fa4046ba39550c6a3cae2fffd9d27228e280
cosmic.gao <cosmic.gao@outlook.com>
2025-10-15 19:01:00 +0800

    新增Heap 实例方法

--- queue/index.ts
+++ queue/index.ts
@@ -13,12 +13,17 @@ export class BinaryHeap<T> {
         return this.heap[0];
     }
 
-    public pop(): T | undefined {
-        const last = this.heap.pop()
-        if (this.size > 0 && last !== undefined) {
+    public poll(): T | undefined {
+        if (this.size === 0) return undefined;
 
+        const top = this.heap[0]
+
+        const last = this.heap.pop()!
+        if (this.size > 0) {
+            this.heap[0] = last
+            this.heapifyDown()
         }
-        return last
+        return top
     }
 
     public push(node: T): number {
@@ -40,7 +45,25 @@ export class BinaryHeap<T> {
     }
 
     private heapifyDown(index: number = 0) {
+        while (this.hasLeftChild(index)) {
+            let smallest = this.getLeftChildIndex(index);
 
+            if (
+                this.hasRightChild(index) &&
+                this.comparator(
+                    this.heap[this.getRightChildIndex(index)],
+                    this.heap[smallest]
+                ) < 0
+            ) {
+                smallest = this.getRightChildIndex(index);
+            }
+
+            if (this.comparator(this.heap[index], this.heap[smallest]) <= 0)
+                break;
+
+            this.swap(index, smallest);
+            index = smallest;
+        }
     }
 
     private swap(i: number, j: number) {
@@ -58,6 +81,30 @@ export class BinaryHeap<T> {
     private getParentIndex(index: number) {
         return index - 1 >> 1;
     }
+
+    private hasLeftChild(index: number) {
+        return this.getLeftChildIndex(index) < this.size;
+    }
+
+    private getLeftChild(index: number) {
+        return this.heap[this.getLeftChildIndex(index)];
+    }
+
+    private getLeftChildIndex(index: number) {
+        return (index << 1) + 1;
+    }
+
+    private hasRightChild(index: number) {
+        return this.getRightChildIndex(index) < this.size;
+    }
+
+    private getRightChild(index: number) {
+        return this.heap[this.getRightChildIndex(index)];
+    }
+
+    private getRightChildIndex(index: number) {
+        return (index << 1) + 2;
+    }
 }
 
 const binary = new BinaryHeap<number>((a, b) => a - b);
@@ -67,4 +114,6 @@ binary.push(30)
 binary.push(60)
 binary.push(10)
 binary.push(100)
+binary.push(30)
+binary.push(20)
 console.log(binary.heap)
