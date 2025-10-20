bc7ecee5e88190f59b9b1384714526d7dd11052c
cosmic.gao <cosmic.gao@outlook.com>
2025-10-20 19:00:35 +0800

    构建基础Graph

--- graph/index.ts
+++ graph/index.ts
@@ -1,3 +1,28 @@
-export class Graph {
+export interface Endpoint {
+    name: string;
+}
 
+export interface EndpointHandle extends Endpoint {
+    nodeId: string;
+}
+
+export interface Edge {
+    readonly id: string;
+    source: EndpointHandle;
+    target: EndpointHandle;
+}
+
+export interface Node {
+    readonly id: string;
+    inputs: Endpoint[];
+    outputs: Endpoint[];
+}
+
+export class Graph<N extends Node, E extends Edge> {
+    public readonly nodes: Map<string, N> = new Map();
+    public readonly edges: Map<string, E> = new Map();
+
+    public addNode(node: N) { }
+
+    public addEdge(edge: E) { }
 }
